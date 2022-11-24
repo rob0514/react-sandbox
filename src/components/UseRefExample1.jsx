d37e9d3b25834b47817a41c81f83bca5f53e287d
Rob <babblespring@gmail.com>
2022-11-24 13:52:23 -1000

    First Commit

---
+++ src/components/UseRefExample1.jsx
@@ -0,0 +1,13 @@
+const UseRefExample1 = () => {
+    return (
+        <div>
+           <form>
+               <label htmlfor="name">Name</label>
+               <input type="text" id="name" className="form-control mb-2"/>
+               <button type="submit" className="btn btn-primary">Submit</button>
+           </form>
+        </div>
+    );
+}
+
+export default UseRefExample1;
